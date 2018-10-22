591b77df2a91a37da0eabba6ac1bd8807d58d574
booleanChoice <booleanchoice@163.com>
2018-10-22 23:08:59 +0800

    逐层打印二叉树节点

---
+++ src/main/java/com/booleanchoice/exercise/binarytree/PrintEveryLevel.java
@@ -0,0 +1,39 @@
+package com.booleanchoice.exercise.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 逐层打印二叉树节点，要求带上层数信息
+ */
+public class PrintEveryLevel {
+
+    public void print(Node head) {
+        if (head == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(head);
+        Node last = head;
+        Node nLast = null;
+        int level = 1;
+        System.out.print("Level " + level++ + " : ");
+        while (!queue.isEmpty()) {
+            head = queue.poll();
+            System.out.print(head.value + " ");
+            if (head.left != null) {
+                queue.offer(head.left);
+                nLast = head.left;
+            }
+            if (head.right != null) {
+                queue.offer(head.right);
+                nLast = head.right;
+            }
+            if (head == last && !queue.isEmpty()) {
+                System.out.print("\nLevel " + level++ + " : ");
+                last = nLast;
+            }
+        }
+    }
+
+}
